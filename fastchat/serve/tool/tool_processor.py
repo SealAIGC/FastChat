db467f236ad6c2bc90b84fa7e76f962760dd0923
charlie890414 <charlie890414@gmail.com>
2023-12-05 10:15:02 +0800

    update tool response

--- fastchat/serve/tool/tool_processor.py
+++ fastchat/serve/tool/tool_processor.py
@@ -61,7 +61,7 @@ class toolProcessor:
             template["choices"][0]["delta"]["content"] = "<tool>" + json.dumps(response) + "</tool>"
 
             self.chat_message[-1]["content"] = self.chat_message[-1]["content"].replace(
-                self.tool_message, response["result"]
+                self.tool_message, json.loads(response["result"])['patientInfo']
             )
             self.last_check_index = len(self.chat_message[-1]["content"])
 
